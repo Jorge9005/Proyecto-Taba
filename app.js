4cd4657c62f3be62d512cfa359a10a9d52b2f766
JORGE LUIS SANDOVAL PEREZ <jorge.sandovalpe@uanl.edu.mx>
2021-11-21 15:00:51 -0600

    prueba

---
+++ app.js
@@ -0,0 +1,12 @@
+document.write("<h1>Hello World</h1>");
+console.log('This is a string');
+console.log(1333);
+console.log(true);
+console.log([1,2,3]);
+console.log({'username': 'ryan', 'score': 70.4} );
+
+var username = "john";
+
+console.log(username)
+
+//username
