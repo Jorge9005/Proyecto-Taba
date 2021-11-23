c620ffba62a9185fa960e72b8ddfbfebcb2e84b3
Jorge9005 <92350041+Jorge9005@users.noreply.github.com>
2021-11-23 11:21:16 -0600

    Delete Proyecto-Taba directory

--- Proyecto-Taba/js/scripts.js
+++
@@ -1,8 +0,0 @@
-let menu = document.getElementById("menu");
-
-let menu_bar=document.getElementById("menu-bar");
-
-menu_bar.addEventListener('click', function(){
-    
-    menu.classList.toggle('menu-toggle')
-})
